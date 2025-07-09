d2d7997e2aa7b210f40b556c76d4fc0b7dc05d4a
Debdip Mukherjee <151666336+DebdipWritesCode@users.noreply.github.com>
2025-07-09 16:16:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { paintingData } from "./data";
 //Audio
 const bgAudio = new Audio("./audio/bg.mp3");
 bgAudio.loop = true;
-bgAudio.volume = 0.3;
+bgAudio.volume = 0.0;
 
 const scene = new THREE.Scene();
 
